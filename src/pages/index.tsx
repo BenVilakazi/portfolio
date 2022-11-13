4af8d47fb717399f52c3bb7ff4e06294675cfd1c
Ben Vilakazi <116912017+codexblue@users.noreply.github.com>
2022-11-13 06:56:40 -0800

    install framer

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import AnimatedLogo from '../components/AnimatedLogo';
+import styles from '../styles/pages/Home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+      <title>Ben Vilakazi</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.container}>
+      <AnimatedLogo />
+      </div>
+    </>
+  ); 
+}
